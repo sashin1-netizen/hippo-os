50359d845cff05cdcdc34b4002259435b96ded4b
sashin1-netizen <sashinramdhanee1@gmail.com>
2026-08-23 23:02:47 +0200

    feat: add utility decision brain

---
+++ Source/HippoOS/Public/HippoBrainComponent.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HippoTypes.h"
+#include "HippoBrainComponent.generated.h"
+
+class UHippoNeedsComponent;
+class UHippoPersonalityComponent;
+class UHippoMemoryComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FHippoActionSelected, EHippoAction, Action, float, Score);
+
+UCLASS(ClassGroup=(Hippo), meta=(BlueprintSpawnableComponent))
+class HIPPOOS_API UHippoBrainComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UHippoBrainComponent();
+    virtual void BeginPlay() override;
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    UPROPERTY(BlueprintReadOnly, Category="Hippo|Brain") EHippoAction CurrentAction = EHippoAction::Idle;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Hippo|Brain") float DecisionIntervalSeconds = 2.0f;
+    UPROPERTY(BlueprintAssignable) FHippoActionSelected OnActionSelected;
+
+    UFUNCTION(BlueprintCallable) void DecideNow();
+    UFUNCTION(BlueprintPure) float ScoreAction(EHippoAction Action) const;
+
+private:
+    UPROPERTY() TObjectPtr<UHippoNeedsComponent> Needs;
+    UPROPERTY() TObjectPtr<UHippoPersonalityComponent> Personality;
+    UPROPERTY() TObjectPtr<UHippoMemoryComponent> Memory;
+    float DecisionClock = 0.0f;
+};
