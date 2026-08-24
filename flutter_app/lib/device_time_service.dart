1b8b8d9b96829d118e0634574c42f73c906112ed
sashin1-netizen <sashinramdhanee1@gmail.com>
2026-08-24 02:13:04 +0200

    Send exact local clock data to sanctuary engine

--- flutter_app/lib/device_time_service.dart
+++ flutter_app/lib/device_time_service.dart
@@ -5,16 +5,25 @@ class DeviceClockSnapshot {
     required this.ianaZone,
     required this.localIso8601,
     required this.utcOffsetMinutes,
+    required this.localHour,
+    required this.localMinute,
+    required this.epochMilliseconds,
   });
 
   final String ianaZone;
   final String localIso8601;
   final int utcOffsetMinutes;
+  final int localHour;
+  final int localMinute;
+  final int epochMilliseconds;
 
   Map<String, Object> toEnginePayload() => <String, Object>{
         'iana_zone': ianaZone,
         'local_iso8601': localIso8601,
         'utc_offset_minutes': utcOffsetMinutes,
+        'local_hour': localHour,
+        'local_minute': localMinute,
+        'epoch_milliseconds': epochMilliseconds,
       };
 }
 
@@ -28,6 +37,9 @@ class DeviceTimeService {
       ianaZone: zone.identifier,
       localIso8601: now.toIso8601String(),
       utcOffsetMinutes: now.timeZoneOffset.inMinutes,
+      localHour: now.hour,
+      localMinute: now.minute,
+      epochMilliseconds: now.millisecondsSinceEpoch,
     );
   }
 }
