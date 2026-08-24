638482eee44bff068d188b852496cb0b65bd8870
sashin1-netizen <sashinramdhanee1@gmail.com>
2026-08-24 02:16:07 +0200

    Add Flutter to Godot runtime bridge

---
+++ flutter_app/native_overrides/HippoFlutterPlugin.kt
@@ -0,0 +1,43 @@
+package com.sashin.hippo_os
+
+import org.godotengine.godot.Godot
+import org.godotengine.godot.plugin.GodotPlugin
+import org.godotengine.godot.plugin.SignalInfo
+import org.godotengine.godot.plugin.UsedByGodot
+
+class HippoFlutterPlugin(
+    godot: Godot,
+    private val statusEmitter: (String) -> Unit,
+) : GodotPlugin(godot) {
+
+    companion object {
+        val CAMERA_MODE_REQUESTED = SignalInfo("camera_mode_requested", String::class.java)
+        val DEVICE_TIME_SYNCED = SignalInfo("device_time_synced", String::class.java)
+        val ANIMAL_ACTION_REQUESTED = SignalInfo("animal_action_requested", String::class.java)
+    }
+
+    override fun getPluginName() = "HippoFlutterBridge"
+
+    override fun getPluginSignals() = setOf(
+        CAMERA_MODE_REQUESTED,
+        DEVICE_TIME_SYNCED,
+        ANIMAL_ACTION_REQUESTED,
+    )
+
+    fun requestCameraMode(mode: String) {
+        emitSignal(CAMERA_MODE_REQUESTED.name, mode)
+    }
+
+    fun syncDeviceTime(json: String) {
+        emitSignal(DEVICE_TIME_SYNCED.name, json)
+    }
+
+    fun requestAnimalAction(action: String) {
+        emitSignal(ANIMAL_ACTION_REQUESTED.name, action)
+    }
+
+    @UsedByGodot
+    fun pushStatus(json: String) {
+        runOnHostThread { statusEmitter(json) }
+    }
+}
