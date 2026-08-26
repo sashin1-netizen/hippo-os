4f70a60d208d251fce76e04634e2bfbd15fdbad4
sashin1-netizen <sashinramdhanee1@gmail.com>
2026-08-26 11:43:37 +0200

    feat: persist learned preferences and priority actions

--- Source/HippoOS/Private/HippoBrainComponent.cpp
+++ Source/HippoOS/Private/HippoBrainComponent.cpp
@@ -20,6 +20,17 @@ void UHippoBrainComponent::BeginPlay()
 void UHippoBrainComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (OverrideRemainingSeconds > 0.0f)
+    {
+        OverrideRemainingSeconds = FMath::Max(0.0f, OverrideRemainingSeconds - DeltaTime);
+        if (OverrideRemainingSeconds <= 0.0f)
+        {
+            DecideNow();
+        }
+        return;
+    }
+
     DecisionClock += DeltaTime;
     if (DecisionClock >= DecisionIntervalSeconds)
     {
@@ -30,6 +41,13 @@ void UHippoBrainComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 
 void UHippoBrainComponent::DecideNow()
 {
+    if (OverrideRemainingSeconds > 0.0f)
+    {
+        CurrentAction = OverrideAction;
+        OnActionSelected.Broadcast(CurrentAction, 1.0f);
+        return;
+    }
+
     static const EHippoAction Candidates[] = {
         EHippoAction::Idle,
         EHippoAction::Wander,
@@ -37,7 +55,9 @@ void UHippoBrainComponent::DecideNow()
         EHippoAction::SeekFood,
         EHippoAction::Sleep,
         EHippoAction::Explore,
-        EHippoAction::Play
+        EHippoAction::Play,
+        EHippoAction::EnterWater,
+        EHippoAction::Zoomies
     };
 
     EHippoAction BestAction = EHippoAction::Idle;
@@ -74,6 +94,8 @@ float UHippoBrainComponent::ScoreAction(EHippoAction Action) const
         case EHippoAction::Sleep: Score = (1.0f - N.Energy) * 1.05f; break;
         case EHippoAction::Explore: Score = (1.0f - N.Curiosity) * 0.65f + N.Energy * 0.20f; break;
         case EHippoAction::Play: Score = N.Energy * 0.35f + N.Curiosity * 0.25f + Bond * 0.15f; break;
+        case EHippoAction::EnterWater: Score = (1.0f - N.Cleanliness) * 0.45f + N.Energy * 0.12f; break;
+        case EHippoAction::Zoomies: Score = N.Energy * 0.30f + N.Curiosity * 0.18f; break;
         default: break;
     }
 
@@ -82,6 +104,33 @@ float UHippoBrainComponent::ScoreAction(EHippoAction Action) const
         Score *= Personality->GetActionMultiplier(Action);
     }
 
+    if (const float* Learned = LearnedActionAffinity.Find(Action))
+    {
+        Score += FMath::Clamp(*Learned, -0.25f, 0.35f);
+    }
+
     Score += FMath::FRandRange(-0.035f, 0.035f);
     return FMath::Max(0.0f, Score);
+}
+
+void UHippoBrainComponent::ReinforceAction(EHippoAction Action, float Amount)
+{
+    float& Affinity = LearnedActionAffinity.FindOrAdd(Action);
+    Affinity = FMath::Clamp(Affinity + Amount, -0.25f, 0.35f);
+}
+
+void UHippoBrainComponent::SetActionOverride(EHippoAction Action, float DurationSeconds)
+{
+    OverrideAction = Action;
+    OverrideRemainingSeconds = FMath::Max(0.05f, DurationSeconds);
+    CurrentAction = Action;
+    DecisionClock = 0.0f;
+    OnActionSelected.Broadcast(CurrentAction, 1.0f);
+}
+
+void UHippoBrainComponent::ClearActionOverride()
+{
+    OverrideRemainingSeconds = 0.0f;
+    DecisionClock = DecisionIntervalSeconds;
+    DecideNow();
 }
